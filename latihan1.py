00274f06e5dd112bacdf8162145e983078517e95
Sindyy05 <143860009+Sindyy05@users.noreply.github.com>
2023-09-02 10:59:33 +0700

    Add files via upload
    upload file

---
+++ latihan1.py
@@ -0,0 +1,36 @@
+nama_saya = str(input("masukkan nama kamu = "))
+
+print(nama_saya, "bertipe", type(nama_saya))
+
+umur = int(input("masukkan umur = "))
+print(umur, "bertipe", type(umur))
+
+IPK = float(input("masukkan ipk mu ="))
+print(IPK, "bertipe", type(IPK))
+
+menikah = bool(input("sudah menikah ="))
+print("menikah?",menikah,  "bertipe", type(menikah))
+
+matkul_diambil1 = ["alpro", "sig"]
+print("matkul : ",matkul_diambil1, "bertipe", type(matkul_diambil1))
+
+matkul_diambil2 = ("web", "pemvis")
+print("matkul lain : ",matkul_diambil2, "bertipe", type(matkul_diambil2))
+
+matkul_diambil3 = {"alpro", "alpro", "pemweb", "data science", "data science"}
+print("matkul lain lain : ",matkul_diambil3, "bertipe", type(matkul_diambil3))
+
+print(2 + 6)
+print(2 * 6)
+print(2 / 6)
+print(2 // 6)
+print(2 - 6)
+print(2 % 6)
+
+nama_universitas = "universitas trunojoyo"
+print(nama_universitas)
+
+nama_universitas = str("masukkan nama universitas")
+print(nama_universitas)
+
+
